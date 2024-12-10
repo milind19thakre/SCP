fb6b4907c037711c03faecc55e4c3ef3c07497f0
milind <milind19thakre@gmail.com>
2024-12-10 21:31:15 +0530

    Commit Success

--- src/test/java/testslayer/LoginTest.java
+++ src/test/java/testslayer/LoginTest.java
@@ -5,8 +5,10 @@ import pagelayer.ScpLoginPage;
 
 public class LoginTest extends ScpLoginPage {
 
-    @Test
+    //Test
+	@Test
     public void loginTest() {
+		System.out.println("Login test Begin");
         login("implementation", "Bluer1dge@3"); // Call the login method from ScpLoginPage
     }
 }
